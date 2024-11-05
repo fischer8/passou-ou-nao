a92420fe5341bd8fb92908b07c4f88dd8212ed9e
Henrique Fischer <104164663+fischer8@users.noreply.github.com>
2024-11-05 03:03:06 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => resolve(Number(answer)));
+  });
+};
+
+(async () => {
+  console.log("")
+  console.log("------ ATENÇÃO ------")
+  console.log("Utilize o ponto invés da virgula para representar casas decimais")
+  console.log("Se não tiver feito alguma prova coloque a nota 0")
+  console.log("")
+  console.log("Primeiro semestre")
+  let cp1 = await askQuestion('Cp1: ');
+  cp1 += await askQuestion('Cp2: ');
+  cp1 += await askQuestion('Sprint1: ');
+  cp1 += await askQuestion('Sprint2: ');
+  const gs1 = await askQuestion('Nota gs: ');
+  const primeiroSem = (((cp1 / 4) * 10) * 0.4) + (gs1 * 0.6)
+  console.log("")
+  console.log("Segundo semestre")
+  let cp2 = await askQuestion('Cp3: ');
+  cp2 += await askQuestion('Cp4: ');
+  cp2 += await askQuestion('Sprint3: ');
+  cp2 += await askQuestion('Sprint4: ');
+  const gs2 = await askQuestion('Nota gs: ');
+  const segundoSem = (((cp2 / 4) * 10) * 0.4) + (gs2 * 0.6)
+  console.log("")
+  const mediaFinal = (primeiroSem * 0.4) + (segundoSem * 0.6)
+  console.log("Média primeiro semestre: " + primeiroSem.toFixed(2))
+  console.log("Média segundo semestre: " + segundoSem.toFixed(2))
+  console.log("Média final: " + mediaFinal.toFixed(2))
+  console.log("")
+  if (mediaFinal >= 60) {
+    console.log("Passou de ano parabens")
+  } else {
+    console.log("Não passou ainda")
+  }
+  rl.close();
+})();
